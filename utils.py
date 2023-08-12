dded477222eadaccc199d26ac02aed867073664d
hanson.yun <hanson.yun@qq.com>
2023-08-12 19:06:51 +0800

    add gan implement

--- utils.py
+++ utils.py
@@ -1,7 +1,8 @@
 import torch
 import numpy as np
+import visdom
 from matplotlib import pyplot as plt
-
+import random
 
 def plot_curve(data):
     fig = plt.figure()
@@ -38,3 +39,70 @@ def L1_penalty(parameters):
     for param in parameters:
         regularization_loss += torch.sum(torch.abs(param))
     return regularization_loss
+
+def gan_generate_image(D,G,xr,epoch,viz):
+    N_POINTS = 128
+    RANGE = 3
+    BATCH_SIZE = 512
+    plt.clf()
+
+    points = np.zeros((N_POINTS, N_POINTS, 2), dtype='float32')
+    points[:, :, 0] = np.linspace(-RANGE, RANGE, N_POINTS)[:, None]
+    points[:, :, 1] = np.linspace(-RANGE, RANGE, N_POINTS)[None, :]
+    points = points.reshape((-1, 2))
+    # (16384, 2)
+    # print('p:', points.shape)
+
+    # draw contour
+    with torch.no_grad():
+        points = torch.Tensor(points).cuda() # [16384, 2]
+        disc_map = D(points).cpu().numpy() # [16384]
+    x = y = np.linspace(-RANGE, RANGE, N_POINTS)
+    cs = plt.contour(x, y, disc_map.reshape((len(x), len(y))).transpose())
+    plt.clabel(cs, inline=1, fontsize=10)
+    # plt.colorbar()
+
+
+    # draw samples
+    with torch.no_grad():
+        z = torch.randn(BATCH_SIZE, 2).cuda() # [b, 2]
+        samples = G(z).cpu().numpy() # [b, 2]
+    plt.scatter(xr[:, 0], xr[:, 1], c='orange', marker='.')
+    plt.scatter(samples[:, 0], samples[:, 1], c='green', marker='+')
+
+    viz.matplot(plt, win='contour', opts=dict(title='p(x):%d'%epoch))
+
+
+def gan_data_generator(batch_size=512):
+    # 缩放比例
+    scale = 2
+    # 定义了8个随机分布的中心点
+    centers = [
+        (1,0),
+        (-1,0),
+        (0,1),
+        (0,-1),
+        (1./np.sqrt(2), 1./np.sqrt(2)),
+        (1./np.sqrt(2), -1./np.sqrt(2)),
+        (-1./np.sqrt(2), 1./np.sqrt(2)),
+        (-1./np.sqrt(2), -1./np.sqrt(2))
+    ]
+
+    centers = [(scale*x, scale*y) for x, y in centers]
+
+    while True:
+        dataset = []
+
+        for i in range(batch_size):
+            point = np.random.randn(2)*.02
+            center = random.choice(centers)
+            # 在center附近生成随机点, 随机值服从正态分布
+            point[0] += center[0]
+            point[1] += center[1]
+            dataset.append(point)
+
+        dataset = np.array(dataset, dtype='float32')
+        dataset /= 1.414
+        yield dataset # yield是一个生成器函数，每次调用都会返回一个生成器对象，而不是函数的返回值
+
+
